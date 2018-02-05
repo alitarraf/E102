59aad4e05e3654015925f17ff762e07403dd19d8
Ali Tarraf <ali.tarraf@gmail.com>
2018-02-05 18:14:05 +0200

    commented print output lines

--- catmullrom.py
+++ catmullrom.py
@@ -27,13 +27,13 @@ def CatmullRomSpline(P0, P1, P2, P3, nPoints=100):
   # Reshape so that we can multiply by the points P0 to P3
   # and get a point for each value of t.
   t = t.reshape(len(t),1)
-  print(t)
+  #print(t)
   A1 = (t1-t)/(t1-t0)*P0 + (t-t0)/(t1-t0)*P1
   A2 = (t2-t)/(t2-t1)*P1 + (t-t1)/(t2-t1)*P2
   A3 = (t3-t)/(t3-t2)*P2 + (t-t2)/(t3-t2)*P3
-  print(A1)
-  print(A2)
-  print(A3)
+  #print(A1)
+  #print(A2)
+  #print(A3)
   B1 = (t2-t)/(t2-t0)*A1 + (t-t0)/(t2-t0)*A2
   B2 = (t3-t)/(t3-t1)*A2 + (t-t1)/(t3-t1)*A3
 
